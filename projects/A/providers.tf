6827bc799c795fcd4659e512f082d7a0efeb0066
Rsree28 <roja.sree28@gmail.com>
2020-01-09 13:05:50 +0530

    assumerole added

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region                  = "us-east-1"
   #shared_credentials_file = "/Users/roja/.aws/credentials"
-  profile                 = "roja"
+  assume_role{
+    role_arn = "arn:aws:iam::757563985248:user/roja"
+  }
 
 }
