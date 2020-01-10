d8819cebde9dda9cbfdb8461e7abe7889cddb4e2
Rsree28 <roja.sree28@gmail.com>
2020-01-10 11:44:08 +0530

    add sharedcreds

--- projects/A/providers.tf
+++ projects/A/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                  = "us-east-1"
-  #shared_credentials_file = "/Users/roja/.aws/credentials"
+  shared_credentials_file = "/Users/roja/.aws/credentials"
   assume_role{
     role_arn = "arn:aws:iam::757563985248:user/roja"
   }
